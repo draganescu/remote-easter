c4231d15bf5bc6b37fae9d32b0996369da4cf08c
Adrian Florescu <florescuadyz@yahoo.com>
2021-04-04 16:33:59 +0300

    Updated package version, import method and changed the key

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 
 // Add the Firebase products that you want to use
 import "firebase/firestore";
@@ -7,13 +7,15 @@ import "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // Andrei Pet's firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAeHT25wUYbw3GtEFyhiSj4bYCYvQCZxpE",
-    authDomain: "eggster.firebaseapp.com",
-    databaseURL: "https://eggster.firebaseio.com",
-    projectId: "eggster",
-    storageBucket: "eggster.appspot.com",
-    messagingSenderId: "899598577489",
-    appId: "1:899598577489:web:331ca666050417bdb66d70"
+    apiKey: "AIzaSyBTjMGkj1vbTcWTJTawLMlYM-EH6o2hB50",
+    authDomain: "easter-2021.firebaseapp.com",
+    databaseURL:
+        "https://easter-2021-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "easter-2021",
+    storageBucket: "easter-2021.appspot.com",
+    messagingSenderId: "501908955059",
+    appId: "1:501908955059:web:d2a7b3ea8274009dbab5c6",
+    measurementId: "G-3DK8562BQR"
 };
   
 
